1ba3d3ba1413248f8d0885f01f7b52c5a8a60589
israelhrm1995 <isrealhrm1995@gmail.com>
2025-01-25 17:01:06 -0300

    tratando excecao para obj nao encontrado

--- src/main/java/br/com/israel/Junit/api_Junit/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/israel/Junit/api_Junit/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.israel.Junit.api_Junit.services.impl;
 import br.com.israel.Junit.api_Junit.domain.User;
 import br.com.israel.Junit.api_Junit.repositories.UserRepository;
 import br.com.israel.Junit.api_Junit.services.UserService;
+import br.com.israel.Junit.api_Junit.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj =  repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(()->new ObjectNotFoundException("Objeto nao encontrado"));
     }
 }
