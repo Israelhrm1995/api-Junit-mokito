6f83dbc242dc8d1d40ac46d3fb60882c1bb9fc07
israelhrm1995 <isrealhrm1995@gmail.com>
2025-01-25 11:58:50 -0300

    Iniciando projeto

---
+++ src/test/java/br/com/israel/Junit/api_Junit/ApiJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.israel.Junit.api_Junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
